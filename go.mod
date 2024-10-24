b7f86290738832415eb427ab40b2a67038e36ed8
Igor Crevar <crewce@gmail.com>
2024-10-24 12:01:52 +0200

    new gouroboros

--- go.mod
+++ go.mod
@@ -3,23 +3,24 @@ module github.com/Ethernal-Tech/cardano-infrastructure
 go 1.21
 
 require (
-	github.com/blinklabs-io/gouroboros v0.79.0
-	github.com/fxamacker/cbor/v2 v2.6.0
+	github.com/blinklabs-io/gouroboros v0.101.0
+	github.com/fxamacker/cbor/v2 v2.7.0
 	github.com/hashicorp/go-hclog v1.6.3
 	github.com/quasilyte/go-ruleguard v0.4.2
 	github.com/quasilyte/go-ruleguard/dsl v0.3.22
-	github.com/stretchr/testify v1.8.4
+	github.com/stretchr/testify v1.9.0
 	github.com/syndtr/goleveldb v1.0.0
-	github.com/utxorpc/go-codegen v0.5.0
+	github.com/utxorpc/go-codegen v0.11.0
 	go.etcd.io/bbolt v1.3.9
-	golang.org/x/crypto v0.22.0
-	golang.org/x/tools v0.18.0
+	golang.org/x/crypto v0.28.0
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d
 )
 
 require (
 	cloud.google.com/go/compute v1.25.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v1.1.7 // indirect
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
@@ -54,11 +55,11 @@ require (
 	go.opentelemetry.io/otel/metric v1.24.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	golang.org/x/exp/typeparams v0.0.0-20240213143201-ec583247a57a // indirect
-	golang.org/x/mod v0.15.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/oauth2 v0.17.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/api v0.169.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
@@ -71,17 +72,17 @@ require (
 require (
 	cloud.google.com/go/secretmanager v1.12.0
 	github.com/aws/aws-sdk-go v1.51.18
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/hashicorp/vault/api v1.12.2
 	github.com/jinzhu/copier v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	golang.org/x/sys v0.19.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	"github.com/Ethernal-Tech/cardano-infrastructure/indexer"
+	"github.com/Ethernal-Tech/cardano-infrastructure/indexer/db"
+	"github.com/Ethernal-Tech/cardano-infrastructure/logger"
+	"github.com/hashicorp/go-hclog"
+)
+
+func startSyncer(ctx context.Context, isVector bool, id int, baseDirectory string) error {
+	var (
+		address             string
+		networkMagic        uint32
+		addressesOfInterest []string
+	)
+
+	if isVector {
+		address = "localhost:5200"
+		networkMagic = uint32(1127)
+		addressesOfInterest = []string{}
+	} else {
+		address = "localhost:5100"
+		networkMagic = uint32(3311)
+		addressesOfInterest = []string{}
+	}
+
+	logger, err := logger.NewLogger(logger.LoggerConfig{
+		LogLevel:      hclog.Debug,
+		JSONLogFormat: false,
+		AppendFile:    true,
+		LogFilePath:   filepath.Join(baseDirectory, fmt.Sprintf("logs-%d.log", id)),
+	})
+	if err != nil {
+		return err
+	}
+
+	dbs, err := db.NewDatabaseInit("", filepath.Join(baseDirectory, fmt.Sprintf("burek-%d.db", id)))
+	if err != nil {
+		return err
+	}
+
+	confirmedBlockHandler := func(confirmedBlock *indexer.CardanoBlock, txs []*indexer.Tx) error {
+		logger.Info("Confirmed block",
+			"hash", hex.EncodeToString(confirmedBlock.Hash[:]), "slot", confirmedBlock.Slot,
+			"allTxs", len(confirmedBlock.Txs), "ourTxs", len(txs))
+
+		unprocessedTxs, err := dbs.GetUnprocessedConfirmedTxs(0)
+		if err != nil {
+			return err
+		}
+
+		for _, tx := range unprocessedTxs {
+			logger.Info("Tx has been processed", "tx", tx)
+			for _, ot := range tx.Outputs {
+				logger.Info("output", "addr", ot.Address, "amount", ot.Amount, "slot", ot.Slot)
+			}
+		}
+
+		return dbs.MarkConfirmedTxsProcessed(unprocessedTxs)
+	}
+
+	indexerConfig := &indexer.BlockIndexerConfig{
+		StartingBlockPoint: &indexer.BlockPoint{
+			BlockSlot:   0,
+			BlockHash:   [32]byte{},
+			BlockNumber: 0,
+		},
+		AddressCheck:            indexer.AddressCheckAll,
+		ConfirmationBlockCount:  10,
+		AddressesOfInterest:     addressesOfInterest,
+		SoftDeleteUtxo:          false,
+		KeepAllTxOutputsInDB:    false,
+		KeepAllTxsHashesInBlock: true,
+	}
+	syncerConfig := &indexer.BlockSyncerConfig{
+		NetworkMagic:   networkMagic,
+		NodeAddress:    address,
+		RestartOnError: true,
+		RestartDelay:   time.Second * 2,
+		KeepAlive:      true,
+	}
+
+	indexerObj := indexer.NewBlockIndexer(indexerConfig, confirmedBlockHandler, dbs, logger.Named("block_indexer"))
+	syncer := indexer.NewBlockSyncer(syncerConfig, indexerObj, logger.Named("block_syncer"))
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			syncer.Close()
+			dbs.Close()
+		case err := <-syncer.ErrorCh():
+			logger.Error("syncer fatal err", "err", err)
+
+			dbs.Close()
+		}
+	}()
+
+	err = syncer.Sync()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	syncerTimeout := time.Second * 50
+	sequenceCount := 10
+
+	baseDirectory, err := os.MkdirTemp("", "syncer-test")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		os.RemoveAll(baseDirectory)
+		os.Remove(baseDirectory)
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	signalChannel := make(chan os.Signal, 1)
+	// Notify the signalChannel when the interrupt signal is received (Ctrl+C)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+
+	for i := 1; i <= sequenceCount; i++ {
+		fmt.Println("starting syncer ", i, baseDirectory)
+
+		timeOutContext, cancel := context.WithCancel(ctx)
+
+		if err := startSyncer(timeOutContext, i&1 == 1, i, baseDirectory); err != nil {
+			fmt.Println("syncer error", err)
+		}
+
+		select {
+		case <-signalChannel:
+			cancel()
+
+			return
+		case <-ctx.Done():
+			cancel()
+
+			return
+		case <-time.After(syncerTimeout):
+			fmt.Println("stopping syncer")
+
+			cancel()
+		}
+	}
+}
