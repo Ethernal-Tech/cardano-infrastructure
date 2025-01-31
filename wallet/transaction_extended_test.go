93f3803efa345cc048a12e2f6ed484153e72d432
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2025-01-31 10:51:44 +0100

    token cost sum

---
+++ wallet/transaction_extended_test.go
@@ -0,0 +1,65 @@
+package wallet
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetTokenCostSum(t *testing.T) {
+	t.Parallel()
+
+	token1, _ := NewTokenWithFullName("29f8873beb52e126f207a2dfd50f7cff556806b5b4cba9834a7b26a8.4b6173685f546f6b656e", true)
+	token2, _ := NewTokenWithFullName("29f8873beb52e126f207a2dfd50f7cff556806b5b4cba9834a7b26a8.Route3", false)
+	token3, _ := NewTokenWithFullName("29f8873beb52e126f207a2dfd50f7cff556806b5b4cba9834a7b26a8.Route345", false)
+
+	tokenAmount1 := NewTokenAmount(token1, 11_000_039)
+	tokenAmount2 := NewTokenAmount(token2, 236_872_039)
+	tokenAmount3 := NewTokenAmount(token3, 12_236_872_039)
+
+	txBuilder, err := NewTxBuilder(ResolveCardanoCliBinary(MainNetNetwork))
+	require.NoError(t, err)
+
+	defer txBuilder.Dispose()
+
+	txBuilder.SetProtocolParameters(protocolParameters)
+
+	address := "addr_test1vqjysa7p4mhu0l25qknwznvj0kghtr29ud7zp732ezwtzec0w8g3u"
+	utxos := []Utxo{
+		{
+			Amount: 100_000_000,
+		},
+		{
+			Amount: 0,
+			Tokens: []TokenAmount{
+				tokenAmount1,
+				tokenAmount2,
+			},
+		},
+		{
+			Amount: 3_000_000,
+			Tokens: []TokenAmount{
+				tokenAmount3,
+			},
+		},
+	}
+	result, err := GetTokenCostSum(txBuilder, address, utxos)
+	require.NoError(t, err)
+	require.Equal(t, uint64(1293000), result)
+
+	utxos[1].Tokens[0].Amount = 1 // changing token amount will change the output
+	result, err = GetTokenCostSum(txBuilder, address, utxos)
+	require.NoError(t, err)
+	require.Equal(t, uint64(1275760), result)
+
+	utxos[2].Tokens[0].Amount = 3 // changing token amount will change the output
+	result, err = GetTokenCostSum(txBuilder, address, utxos)
+	require.NoError(t, err)
+	require.Equal(t, uint64(1241280), result)
+
+	utxos[0].Amount = 3
+	utxos[1].Amount = 300_021_416_931_256_900 // changing lovelace amounts won't make any difference
+	result, err = GetTokenCostSum(txBuilder, address, utxos)
+	require.NoError(t, err)
+	require.Equal(t, uint64(1241280), result)
+}
