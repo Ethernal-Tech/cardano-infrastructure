af68026199bc9931b85c8d51f06945556b33f563
Igor Crevar <crewce@gmail.com>
2025-05-09 10:47:29 +0200

    logger fixes

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -10,7 +10,52 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_getLogFileWriter(t *testing.T) {
+func TestNewLogger(t *testing.T) {
+	testDir, err := os.MkdirTemp("", "new-logger-test")
+	require.NoError(t, err)
+
+	defer os.RemoveAll(testDir)
+
+	filePath := filepath.Join(testDir, "dummy", "file.log")
+
+	t.Run("rotating empty", func(t *testing.T) {
+		_, err := NewLogger(LoggerConfig{
+			RotatingLogsEnabled: true,
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("rotating with file path", func(t *testing.T) {
+		logger, err := NewLogger(LoggerConfig{
+			RotatingLogsEnabled: true,
+			LogFilePath:         filePath,
+		})
+
+		require.NoError(t, err)
+		require.NotNil(t, logger)
+	})
+
+	t.Run("empty", func(t *testing.T) {
+		logger, err := NewLogger(LoggerConfig{
+			RotatingLogsEnabled: false,
+		})
+
+		require.NoError(t, err)
+		require.NotNil(t, logger)
+	})
+
+	t.Run("with file path", func(t *testing.T) {
+		logger, err := NewLogger(LoggerConfig{
+			RotatingLogsEnabled: false,
+			LogFilePath:         filePath,
+		})
+
+		require.NoError(t, err)
+		require.NotNil(t, logger)
+	})
+}
+
+func TestGetLogFileWriter(t *testing.T) {
 	testDir, err := os.MkdirTemp("", "logger-test")
 	require.NoError(t, err)
 
@@ -20,30 +65,30 @@ func Test_getLogFileWriter(t *testing.T) {
 	filePathWithoutExtension := filepath.Join(testDir, "dummy2", "file")
 
 	t.Run("empty", func(t *testing.T) {
-		f, err := getLogFileWriter(" ", true)
+		f, err := getLogFileWriter(LoggerConfig{AppendFile: true})
 		require.NoError(t, err)
 		require.Nil(t, f)
 	})
 
 	t.Run("with append", func(t *testing.T) {
-		f, err := getLogFileWriter(filePathWithExtension, true)
+		f, err := getLogFileWriter(LoggerConfig{LogFilePath: filePathWithExtension, AppendFile: true})
 		require.NoError(t, err)
 		require.NotNil(t, f)
 		require.Equal(t, filePathWithExtension, f.Name())
 
-		f, err = getLogFileWriter(filePathWithoutExtension, true)
+		f, err = getLogFileWriter(LoggerConfig{LogFilePath: filePathWithoutExtension, AppendFile: true})
 		require.NoError(t, err)
 		require.NotNil(t, f)
 		require.Equal(t, filePathWithoutExtension, f.Name())
 	})
 
 	t.Run("without append", func(t *testing.T) {
-		f, err := getLogFileWriter(filePathWithExtension, false)
+		f, err := getLogFileWriter(LoggerConfig{LogFilePath: filePathWithExtension, AppendFile: false})
 		require.NoError(t, err)
 		require.NotNil(t, f)
 		require.Regexp(t, regexp.MustCompile(fmt.Sprintf("^%s/dummy1/file_.*\\.log$", testDir)), f.Name())
 
-		f, err = getLogFileWriter(filePathWithoutExtension, false)
+		f, err = getLogFileWriter(LoggerConfig{LogFilePath: filePathWithoutExtension, AppendFile: false})
 		require.NoError(t, err)
 		require.NotNil(t, f)
 		require.Regexp(t, regexp.MustCompile(fmt.Sprintf("^%s/dummy2/file_.*$", testDir)), f.Name())
